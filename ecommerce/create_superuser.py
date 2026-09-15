1af8e13ae1f79b052832911ee7a8833fd30e26fa
Neeraja-12 <neerajan340@gmail.com>
2026-09-15 09:05:13 +0530

    Add superuser creation script for Render deploy

---
+++ ecommerce/create_superuser.py
@@ -0,0 +1,20 @@
+﻿import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+
+if not password:
+    print('DJANGO_SUPERUSER_PASSWORD not set, skipping superuser creation.')
+elif User.objects.filter(username=username).exists():
+    print(f'Superuser {username} already exists, skipping.')
+else:
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser {username} created.')
